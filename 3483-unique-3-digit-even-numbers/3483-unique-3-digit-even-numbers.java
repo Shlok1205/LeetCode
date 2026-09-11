704d1a0d31c717bfb36de712edac8aee6f5659d3
Shlok Madan <shlokmadan8@gmail.com>
2026-09-11 21:16:45 +0530

    Time: 13 ms (15.2%), Space: 47 MB (7.47%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,45 @@
+class Solution
+{
+    public int totalNumbers(int[] digits)
+    {
+        int[] freq=new int[10];
+
+        for(int x:digits)
+        {
+            freq[x]++;
+        }
+
+        int ans=0;
+
+        for(int num=100;num<=998;num+=2)
+        {
+            int a=num/100;
+            int b=(num/10)%10;
+            int c=num%10;
+
+            int[] need=new int[10];
+
+            need[a]++;
+            need[b]++;
+            need[c]++;
+
+            boolean possible=true;
+
+            for(int i=0;i<10;i++)
+            {
+                if(need[i]>freq[i])
+                {
+                    possible=false;
+                    break;
+                }
+            }
+
+            if(possible)
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
